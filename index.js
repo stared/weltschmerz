ba46ff1c53bd0afd14c3676aab777684d8141f91
Piotr Migdal <pmigdal@gmail.com>
2015-01-25 14:09:14 +0100

    fetching data

--- index.js
+++ index.js
@@ -3,12 +3,34 @@
 var ageMin = 12;
 var ageMax = 20;
 
-//
+var queryBegin = "http://suggestqueries.google.com/complete/search?client=chrome&q=";
+var queryEnd = "&callback=?";
+
+var allSuggestions = [];
 
 d3.select('#go').on('click', function () {
+  allSuggestions = [];
   var query = d3.select('#query').attr('value');
-  $.getJSON("http://suggestqueries.google.com/complete/search?client=chrome&q=i am 15 and I&callback=?", function (data) {
-    console.log(data);
-  })
+  fetchForAge(query, ageMin, ageMax);
 });
 
+function fetchForAge (query, age, ageMax) {
+
+  var queryFilled = query.replace("X", age);
+  console.log("Quering for: ", queryFilled);
+
+  $.getJSON(queryBegin + queryFilled + queryEnd, function (data) {
+    
+    console.log(data[0]);
+    allSuggestions.push({
+      age:         age,
+      suggestions: data[1]
+    });
+
+    if (age < ageMax) {
+      fetchForAge (query, age + 1, ageMax);
+    }
+
+  });
+
+}
